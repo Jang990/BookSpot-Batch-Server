e5be43a27dafcf402959712be753c26382e35478
Jang <sdsd090811@gmail.com>
2024-11-29 22:25:53 +0900

    feat: 프로젝트 설정

---
+++ src/main/java/com/bookspot/batch/bookspot_batch/BookspotBatchApplication.java
@@ -0,0 +1,13 @@
+package com.bookspot.batch.bookspot_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookspotBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookspotBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookspot/batch/bookspot_batch/BookspotBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookspot.batch.bookspot_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookspotBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
