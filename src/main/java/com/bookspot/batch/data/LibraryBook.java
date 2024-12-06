bcddf701da38336fcc1e4d32dad574b0f8355e26
Jang <sdsd090811@gmail.com>
2024-12-06 17:44:05 +0900

    refactor: 도서관 소장 도서 클래스에 롬복 어노테이션 추가

--- src/main/java/com/bookspot/batch/data/LibraryBook.java
+++ src/main/java/com/bookspot/batch/data/LibraryBook.java
@@ -3,9 +3,13 @@ package com.bookspot.batch.data;
 
 import com.bookspot.batch.data.book.Isbn13;
 import com.bookspot.batch.data.library.LibraryCode;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
 public class LibraryBook {
     private Isbn13 isbn13;
     private LibraryCode libraryCode;
