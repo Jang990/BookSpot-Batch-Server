a6f84ed79d5a7827069b83c1ac4b4636c90c228b
Jang <sdsd090811@gmail.com>
2024-12-06 17:36:29 +0900

    feat: 도서관 소장 도서 데이터 클래스 추가

---
+++ src/main/java/com/bookspot/batch/data/LibraryBook.java
@@ -0,0 +1,11 @@
+package com.bookspot.batch.data;
+
+
+import java.time.LocalDate;
+
+public class LibraryBook {
+    private String isbn13;
+    private String libraryCode;
+    private int loanCount;          // 10
+    private LocalDate registrationDate; // 2024-11-29
+}
