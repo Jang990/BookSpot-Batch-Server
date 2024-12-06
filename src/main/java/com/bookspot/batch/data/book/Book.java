188fb65b1d8167a2f08544571dd4086a372e6217
Jang <sdsd090811@gmail.com>
2024-12-06 17:38:48 +0900

    refactor: 도서에서 불필요한 데이터 제거

--- src/main/java/com/bookspot/batch/data/book/Book.java
+++ src/main/java/com/bookspot/batch/data/book/Book.java
@@ -3,18 +3,13 @@ package com.bookspot.batch.data.book;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 public class Book {
     private String isbn13;          // 9788954605816
-    private int volume;             // 3
-    private String title;           // 눈물상자
+    private String title;           // 눈물상자 (제목 + 권 수)
     private String author;          // 한강 글 ;봄로야 그림
     private String publisher;       // 문학동네
     private int publicationYear;    // 2021
-    private int loanCount;          // 10
-    private LocalDate registrationDate; // 2024-11-29
     private String subjectCode; // 813.7
 }
