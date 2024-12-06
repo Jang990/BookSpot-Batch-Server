e15d86a9804e1d92dc563f9f7fc42c6bdc7085f8
Jang <sdsd090811@gmail.com>
2024-12-06 17:42:01 +0900

    refactor: Book의 Isbn13을 String대신 클래스를 사용하도록 변경

--- src/main/java/com/bookspot/batch/data/LibraryBook.java
+++ src/main/java/com/bookspot/batch/data/LibraryBook.java
@@ -1,10 +1,12 @@
 package com.bookspot.batch.data;
 
 
+import com.bookspot.batch.data.book.Isbn13;
+
 import java.time.LocalDate;
 
 public class LibraryBook {
-    private String isbn13;
+    private Isbn13 isbn13;
     private String libraryCode;
     private int loanCount;          // 10
     private LocalDate registrationDate; // 2024-11-29
--- src/main/java/com/bookspot/batch/data/book/Book.java
+++ src/main/java/com/bookspot/batch/data/book/Book.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Book {
-    private String isbn13;          // 9788954605816
+    private Isbn13 isbn13;          // 9788954605816
     private String title;           // 눈물상자 (제목 + 권 수)
     private String author;          // 한강 글 ;봄로야 그림
     private String publisher;       // 문학동네
