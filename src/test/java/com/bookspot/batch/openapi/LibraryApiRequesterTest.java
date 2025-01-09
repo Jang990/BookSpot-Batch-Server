99c7aca2258fc008ddc5cafd5b2d51b242695472
Jang <sdsd090811@gmail.com>
2025-01-09 11:47:30 +0900

    feat: 정보나루 도서관 api를 통한 정보 불러오기 기능 추가

---
+++ src/test/java/com/bookspot/batch/openapi/LibraryApiRequesterTest.java
@@ -0,0 +1,27 @@
+package com.bookspot.batch.openapi;
+
+import com.bookspot.batch.data.library.Library;
+import com.bookspot.batch.library.api.LibraryApiRequester;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+@SpringBootTest
+class LibraryApiRequesterTest {
+
+    @Autowired
+    LibraryApiRequester requester;
+
+//    @Test
+    void 정보나루_도서관_API_테스트() {
+        PageRequest pageable = PageRequest.of(0, 10);
+        List<Library> result = requester.findAllSupportedLibrary(pageable);
+        for (Library library : result) {
+            System.out.println(library.getName());
+        }
+    }
+
+}
