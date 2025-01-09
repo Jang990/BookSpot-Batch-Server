5049759323b37479a9c1d855f560d2d7f3113566
Jang <sdsd090811@gmail.com>
2025-01-09 18:25:42 +0900

    feat: 도서관 정보 DB 저장 ItemWriter 추가

--- src/main/java/com/bookspot/batch/library/LibraryJobConfig.java
+++ src/main/java/com/bookspot/batch/library/LibraryJobConfig.java
@@ -5,17 +5,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
 public class LibraryJobConfig {
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
+    private final DataSource dataSource;
 
     private final LibraryItemReader libraryItemReader;
 
@@ -23,7 +30,7 @@ public class LibraryJobConfig {
     public Job libraryJob() {
         return new JobBuilder("libraryJob2", jobRepository)
                 .start(libraryStep())
-//                .incrementer(new RunIdIncrementer())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
@@ -32,7 +39,25 @@ public class LibraryJobConfig {
         return new StepBuilder("libraryStep", jobRepository)
                 .<Library, Library>chunk(LibraryJobConst.LIBRARY_CHUNK_SIZE, platformTransactionManager)
                 .reader(libraryItemReader)
-                .writer(items -> items.forEach(item -> System.out.println(item.getName())))
+                .writer(libraryWriter())
+                .build();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Library> libraryWriter() {
+        return new JdbcBatchItemWriterBuilder<Library>()
+                .dataSource(dataSource)
+                .sql("""
+                        INSERT INTO library (name, library_code, location) VALUES
+                        (?, ?, ST_GeomFromText(CONCAT('POINT(', ?, ' ', ?, ')'), 4326));
+                        """)
+                .itemPreparedStatementSetter(
+                        (library, ps) -> {
+                            ps.setString(1, library.getName());
+                            ps.setString(2, library.getLibraryCode());
+                            ps.setDouble(3, library.getLatitude());
+                            ps.setDouble(4, library.getLongitude());
+                        })
                 .build();
     }
 
