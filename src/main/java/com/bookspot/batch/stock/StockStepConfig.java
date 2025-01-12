b335392f9be1a5b0264160e038cb32ecf86d10cf
Jang <sdsd090811@gmail.com>
2025-01-12 16:19:01 +0900

    refactor: 간단한 실행을 위해 샘플 파일을 하드코딩

--- src/main/java/com/bookspot/batch/stock/StockStepConfig.java
+++ src/main/java/com/bookspot/batch/stock/StockStepConfig.java
@@ -10,17 +10,21 @@ import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
 @RequiredArgsConstructor
 public class StockStepConfig {
 
+    private static final String TARGET_PATH = "부평구립삼산도서관 장서 대출목록 (2024년 12월).csv";
+    private static final Resource SAMPLE_RESOURCE = new PathMatchingResourcePatternResolver().getResource(TARGET_PATH);
+
     @Bean
     @StepScope
-    public FlatFileItemReader<LibraryStockCsvData> bookStockCsvFileReader(Resource resource) {
+    public FlatFileItemReader<LibraryStockCsvData> bookStockCsvFileReader() {
         return new FlatFileItemReaderBuilder<LibraryStockCsvData>()
                 .name("bookStockCsvFileReader")
-                .resource(resource)
+                .resource(SAMPLE_RESOURCE)
                 .encoding("euc-kr")
                 .lineTokenizer(new StockCsvDelimiterTokenizer())
                 .fieldSetMapper(new StockCsvDataMapper())
