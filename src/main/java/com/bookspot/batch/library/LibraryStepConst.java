fb3b1ea2f4811b0ce9cd34a2ecdcbb71b35b3cfb
Jang <sdsd090811@gmail.com>
2025-01-15 20:35:09 +0900

    refactor: Step을 직접 주입받지 않고 Step을 얻어오는 유틸클래스로 받도록 변경

--- src/main/java/com/bookspot/batch/library/LibraryStepConst.java
+++ src/main/java/com/bookspot/batch/library/LibraryStepConst.java
@@ -1,5 +1,7 @@
 package com.bookspot.batch.library;
 
 public class LibraryStepConst {
+    public static final String STEP_NAME = "libraryStep";
+
     protected static final int LIBRARY_CHUNK_SIZE = 100;
 }
