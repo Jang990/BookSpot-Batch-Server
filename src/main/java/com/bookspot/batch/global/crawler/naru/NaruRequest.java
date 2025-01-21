c56153bccab20ec7d6e8e0c088ca3df0de068373
Jang <sdsd090811@gmail.com>
2025-01-21 20:18:55 +0900

    refactor: NaruRequest가 NaruCommonRequest를 상속받도록 변경

--- src/main/java/com/bookspot/batch/global/crawler/naru/NaruRequest.java
+++ src/main/java/com/bookspot/batch/global/crawler/naru/NaruRequest.java
@@ -1,13 +1,11 @@
 package com.bookspot.batch.global.crawler.naru;
 
 import com.bookspot.batch.global.crawler.naru.exception.NaruRequestValidationException;
-import lombok.EqualsAndHashCode;
 import org.springframework.util.StringUtils;
 
 import java.util.Map;
 
-@EqualsAndHashCode
-public class NaruRequest {
+public class NaruRequest extends NaruCommonRequest {
     private static final String HEADER_KEY_JSESSIONID = "JSESSIONID";
 
     private static final String PAIR_DELIMITER = "&";
@@ -15,42 +13,26 @@ public class NaruRequest {
     private static final String BODY_KEY_CSRF_TOKEN = "_csrf";
     private static final String BODY_KEY_LIBRARY_CODE = "libcode";
 
-    private final String jSessionId;
-    private final String csrfToken;
     private final String libraryCode;
 
     public NaruRequest(String jSessionId, String csrfToken, String libraryCode) {
-        if (isBlank(jSessionId)
-                || isBlank(csrfToken)
-                || isBlank(libraryCode))
+        super(jSessionId, csrfToken);
+        if (isBlank(libraryCode))
             throw new NaruRequestValidationException();
 
-        this.jSessionId = jSessionId;
-        this.csrfToken = csrfToken;
         this.libraryCode = libraryCode;
     }
 
     public String getRequestBody() {
-        return BODY_KEY_CSRF_TOKEN + KEY_VALUE_DELIMITER + csrfToken
+        return BODY_KEY_CSRF_TOKEN + KEY_VALUE_DELIMITER + getCsrfToken()
                     + PAIR_DELIMITER
                 + BODY_KEY_LIBRARY_CODE + KEY_VALUE_DELIMITER + libraryCode;
-    }
-
-    public Map<String, String> getHeader() {
-        return Map.of(HEADER_KEY_JSESSIONID, jSessionId);
     }
 
     private boolean isBlank(String jSessionId) {
         return !StringUtils.hasText(jSessionId);
     }
 
-    protected String getJSessionId() {
-        return jSessionId;
-    }
-
-    protected String getCsrfToken() {
-        return csrfToken;
-    }
 
     protected String getLibraryCode() {
         return libraryCode;
