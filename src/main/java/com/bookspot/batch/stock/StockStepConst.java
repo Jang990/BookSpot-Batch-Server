425e04da549b2b7e59920438b82dbe3ed3a2e442
Jang <sdsd090811@gmail.com>
2025-01-22 12:45:52 +0900

    feat: 도서관 소장 도서 파싱을 위한 정보 불러오기 Reader 추가

--- src/main/java/com/bookspot/batch/stock/StockStepConst.java
+++ src/main/java/com/bookspot/batch/stock/StockStepConst.java
@@ -2,6 +2,8 @@ package com.bookspot.batch.stock;
 
 public class StockStepConst {
     public static final String STEP_NAME = "libraryStockStep";
+    public static final String DOWNLOAD_STEP_NAME = "stockFileDownloadStep";
 
     protected static final int CHUNK_SIZE = 1000;
+    public static final int DOWNLOAD_CHUNK_SIZE = 10;
 }
