5e834ebcd52d557ceb55ab83a4fb8028da35305c
Jang <sdsd090811@gmail.com>
2025-01-22 19:11:05 +0900

    feat: 재고 디렉토리 전체 csv를 읽는 reader 추가

--- src/main/java/com/bookspot/batch/book/BookConfig.java
+++ src/main/java/com/bookspot/batch/book/BookConfig.java
@@ -1,7 +1,5 @@
 package com.bookspot.batch.book;
 
-import com.bookspot.batch.book.data.Book;
-import com.bookspot.batch.book.reader.BookCsvData;
 import com.bookspot.batch.stock.data.LibraryStockCsvData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Step;
@@ -9,13 +7,10 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
-import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,7 +18,7 @@ public class BookConfig {
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
 
-    private final FlatFileItemReader<LibraryStockCsvData> stockCsvFileReader;
+    private final MultiResourceItemReader<LibraryStockCsvData> multiBookStockCsvFileReader;
     private final ItemProcessor<LibraryStockCsvData, LibraryStockCsvData> isbnValidationProcessor;
     private final JdbcBatchItemWriter<LibraryStockCsvData> stockBookWriter;
 
@@ -31,7 +26,7 @@ public class BookConfig {
     public Step bookStep() {
         return new StepBuilder(BookStepConst.STEP_NAME, jobRepository)
                 .<LibraryStockCsvData, LibraryStockCsvData>chunk(BookStepConst.CHUNK_SIZE, platformTransactionManager)
-                .reader(stockCsvFileReader)
+                .reader(multiBookStockCsvFileReader)
                 .processor(isbnValidationProcessor)
                 .writer(stockBookWriter)
                 .build();
