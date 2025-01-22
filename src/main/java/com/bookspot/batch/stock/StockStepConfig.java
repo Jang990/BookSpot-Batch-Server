977905db784b1807bb58dbb2b99df26b536e213f
Jang <sdsd090811@gmail.com>
2025-01-22 12:54:12 +0900

    feat: 도서관 소장 도서 파일 다운로드 Step 구성

--- src/main/java/com/bookspot/batch/stock/StockStepConfig.java
+++ src/main/java/com/bookspot/batch/stock/StockStepConfig.java
@@ -1,13 +1,18 @@
 package com.bookspot.batch.stock;
 
+import com.bookspot.batch.stock.data.CurrentLibrary;
 import com.bookspot.batch.stock.data.LibraryStock;
 import com.bookspot.batch.stock.data.LibraryStockCsvData;
+import com.bookspot.batch.stock.data.StockFileData;
 import com.bookspot.batch.stock.processor.LibraryStockProcessor;
+import com.bookspot.batch.stock.processor.file.StockFileInfoParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +28,10 @@ public class StockStepConfig {
     private final LibraryStockProcessor LibraryStockProcessor;
     private final JdbcBatchItemWriter<LibraryStock> libraryStockWriter;
 
+    private final JdbcPagingItemReader<CurrentLibrary> libraryStockDataReader;
+    private final StockFileInfoParser stockFileInfoParser;
+    private final ItemWriter<StockFileData> stockFileDownloaderWriter;
+
     @Bean
     public Step libraryStockStep() {
         return new StepBuilder(StockStepConst.STEP_NAME, jobRepository)
@@ -32,4 +41,15 @@ public class StockStepConfig {
                 .writer(libraryStockWriter)
                 .build();
     }
+
+    @Bean
+    public Step stockFileDownloadStep() {
+        return new StepBuilder(StockStepConst.DOWNLOAD_STEP_NAME, jobRepository)
+                .<CurrentLibrary, StockFileData>chunk(StockStepConst.DOWNLOAD_CHUNK_SIZE, platformTransactionManager)
+                .reader(libraryStockDataReader)
+                .processor(stockFileInfoParser)
+                .writer(stockFileDownloaderWriter)
+                .build();
+    }
+
 }
