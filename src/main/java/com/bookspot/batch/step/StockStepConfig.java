c186fd462a4a54f5f228cd282dcb7395faf5145d
Jang <sdsd090811@gmail.com>
2025-01-23 14:47:16 +0900

    refactor: 사용하지 않는 재고 step 주석 처리

--- src/main/java/com/bookspot/batch/step/StockStepConfig.java
+++ src/main/java/com/bookspot/batch/step/StockStepConfig.java
@@ -1,19 +1,14 @@
 package com.bookspot.batch.step;
 
 import com.bookspot.batch.data.LibraryForFileParsing;
-import com.bookspot.batch.data.LibraryStock;
-import com.bookspot.batch.data.file.csv.LibraryStockCsvData;
 import com.bookspot.batch.data.crawler.StockFileData;
-import com.bookspot.batch.step.processor.csv.stock.LibraryStockProcessor;
 import com.bookspot.batch.step.processor.crawler.StockFilePathParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
-import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -24,15 +19,15 @@ public class StockStepConfig {
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
 
-    private final MultiResourceItemReader<LibraryStockCsvData> multiBookStockCsvFileReader;
+    /*private final MultiResourceItemReader<LibraryStockCsvData> multiBookStockCsvFileReader;
     private final LibraryStockProcessor LibraryStockProcessor;
-    private final JdbcBatchItemWriter<LibraryStock> libraryStockWriter;
+    private final JdbcBatchItemWriter<LibraryStock> libraryStockWriter;*/
 
     private final JdbcPagingItemReader<LibraryForFileParsing> libraryStockDataReader;
     private final StockFilePathParser stockFilePathParser;
     private final ItemWriter<StockFileData> stockFileDownloaderWriter;
 
-    @Bean
+    /*@Bean
     public Step libraryStockStep() {
         return new StepBuilder(StockStepConst.STEP_NAME, jobRepository)
                 .<LibraryStockCsvData, LibraryStock>chunk(StockStepConst.CHUNK_SIZE, platformTransactionManager)
@@ -40,7 +35,7 @@ public class StockStepConfig {
                 .processor(LibraryStockProcessor)
                 .writer(libraryStockWriter)
                 .build();
-    }
+    }*/
 
     @Bean
     public Step stockFileDownloadStep() {
