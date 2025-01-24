890f1c0186974b6293498ea5aff981535944e417
Jang <sdsd090811@gmail.com>
2025-01-24 11:31:40 +0900

    test: 파일 메타데이터 생성기 테스트 코드 추가

---
+++ src/test/java/com/bookspot/batch/step/writer/file/stock/StockCsvMetadataCreatorTest.java
@@ -0,0 +1,24 @@
+package com.bookspot.batch.step.writer.file.stock;
+
+import com.bookspot.batch.global.file.FileFormat;
+import com.bookspot.batch.global.file.FileMetadata;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StockCsvMetadataCreatorTest {
+
+    @Test
+    void 재고_파일_메타데이터를_반환() {
+        FileMetadata metadata = StockCsvMetadataCreator.create(1L, LocalDate.of(2025, 1, 1));
+
+        assertEquals(FileFormat.CSV, metadata.format());
+        assertEquals("1_2025-01-01", metadata.name());
+        assertEquals("1_2025-01-01.csv",metadata.fullName());
+        assertEquals("bookSpotFiles/stock", metadata.directory());
+        assertEquals("bookSpotFiles/stock/1_2025-01-01.csv", metadata.absolutePath());
+    }
+
+}
