cff98fd999023a6a95d7997e6c1959b0a48b5b05
Jang <sdsd090811@gmail.com>
2025-01-26 16:54:00 +0900

    feat: isbn13 검증 시 숫자 검증 추가

--- src/main/java/com/bookspot/batch/step/processor/csv/IsbnValidator.java
+++ src/main/java/com/bookspot/batch/step/processor/csv/IsbnValidator.java
@@ -5,8 +5,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class IsbnValidator {
     public boolean isInValid(String isbn13) {
-        return isbn13 == null
-                || isbn13.isBlank()
-                || isbn13.length() != 13;
+        if(isbn13 == null || isbn13.isBlank() || isbn13.length() != 13)
+            return true;
+
+        for (int i = 0; i < isbn13.length(); i++) {
+            if (isbn13.charAt(i) < '0' || isbn13.charAt(i) > '9') {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
