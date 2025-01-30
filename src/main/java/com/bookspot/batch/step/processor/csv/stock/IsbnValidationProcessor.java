c0b1deb284b8dcf0d021c9baaac234b8ca0102e5
Jang <sdsd090811@gmail.com>
2025-01-30 15:10:57 +0900

    feat: isbn 처리기에서 메모리 저장소에 존재하면 필터링하는 조건 추가

--- src/main/java/com/bookspot/batch/step/processor/csv/stock/IsbnValidationProcessor.java
+++ src/main/java/com/bookspot/batch/step/processor/csv/stock/IsbnValidationProcessor.java
@@ -2,6 +2,7 @@ package com.bookspot.batch.step.processor.csv.stock;
 
 import com.bookspot.batch.step.processor.csv.IsbnValidator;
 import com.bookspot.batch.data.file.csv.LibraryStockCsvData;
+import com.bookspot.batch.step.service.IsbnMemoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
@@ -12,10 +13,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class IsbnValidationProcessor implements ItemProcessor<LibraryStockCsvData, LibraryStockCsvData> {
     private final IsbnValidator validator;
+    private final IsbnMemoryRepository isbnMemoryRepository;
     @Override
     public LibraryStockCsvData process(LibraryStockCsvData item) throws Exception {
         if (validator.isInValid(item.getIsbn())) {
             log.info("잘못된 ISBN13 -> {}", item);
+            return null;
+        }
+
+        if (isbnMemoryRepository.contains(item.getIsbn())) {
+            log.trace("이미 존재하는 ISBN13 -> {}", item);
             return null;
         }
         return item;
