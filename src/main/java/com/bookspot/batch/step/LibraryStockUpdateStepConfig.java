7eb5c1b369e3236ad09d185fbd87a5232387af87
Jang <sdsd090811@gmail.com>
2025-01-31 12:38:23 +0900

    refactor: 도서관 재고 업데이트 날짜 관리 스텝 통합

--- src/main/java/com/bookspot/batch/step/LibraryStockUpdateStepConfig.java
+++ src/main/java/com/bookspot/batch/step/LibraryStockUpdateStepConfig.java
@@ -9,20 +9,23 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
 public class LibraryStockUpdateStepConfig {
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
+    private final DataSource dataSource;
 
     private final FlatFileItemReader<LibraryStockCsvData> bookStockCsvFileReader;
     private final LibraryStockProcessor libraryStockProcessor;
-    private final JdbcBatchItemWriter<LibraryStock> libraryStockWriter;
 
     @Bean
     public Step libraryStockSyncStep() {
@@ -30,7 +33,26 @@ public class LibraryStockUpdateStepConfig {
                 .<LibraryStockCsvData, LibraryStock>chunk(StockStepConst.CHUNK_SIZE, platformTransactionManager)
                 .reader(bookStockCsvFileReader)
                 .processor(libraryStockProcessor)
-                .writer(libraryStockWriter)
+                .writer(libraryStockWriter())
                 .build();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<LibraryStock> libraryStockWriter() {
+        JdbcBatchItemWriter<LibraryStock> writer = new JdbcBatchItemWriterBuilder<LibraryStock>()
+                .dataSource(dataSource)
+                .sql("""
+                        INSERT IGNORE INTO library_stock
+                        (book_id, library_id)
+                        VALUES(?, ?);
+                        """)
+                .itemPreparedStatementSetter(
+                        (book, ps) -> {
+                            ps.setLong(1, book.getBookId());
+                            ps.setLong(2, book.getLibraryId());
+                        })
+                .assertUpdates(false)
+                .build();
+        return writer;
     }
 }
