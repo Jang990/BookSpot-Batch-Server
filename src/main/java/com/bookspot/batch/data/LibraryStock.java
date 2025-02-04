ee1b729c7c1443724a5d56bfa4e54ab74edf48c0
Jang <sdsd090811@gmail.com>
2025-02-04 19:28:22 +0900

    feat: 도서 재고 엔티티 정보 추가

--- src/main/java/com/bookspot/batch/data/LibraryStock.java
+++ src/main/java/com/bookspot/batch/data/LibraryStock.java
@@ -1,11 +1,29 @@
 package com.bookspot.batch.data;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
 
-@Data
-@AllArgsConstructor
+import java.time.LocalDate;
+
+@Getter
+@ToString
 public class LibraryStock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private Long libraryId;
     private Long bookId;
+
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    public LibraryStock(Long libraryId, Long bookId) {
+        this.libraryId = libraryId;
+        this.bookId = bookId;
+    }
 }
