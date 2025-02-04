1c5b38f1ef4bc93cab57adff91e15d449db5e02e
Jang <sdsd090811@gmail.com>
2025-02-04 19:25:56 +0900

    feat: 도서 정보에 엔티티 정보 추가

--- src/main/java/com/bookspot/batch/data/Book.java
+++ src/main/java/com/bookspot/batch/data/Book.java
@@ -1,27 +1,33 @@
 package com.bookspot.batch.data;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
+@Getter
+@ToString
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String isbn13;          // 9788954605816
     private String title;           // 눈물상자 (제목 + 권 수)
     private String author;          // 한강 글 ;봄로야 그림
     private String publisher;       // 문학동네
     private Integer publicationYear;    // 2021
-    private String subjectCode; // 813.7
+    private String classification; // 813.7
     private String volumeName;
 
-    public Book(String isbn13, String title, String author, Integer publicationYear, String subjectCode, String volumeName) {
+    public Book(String isbn13, String title, String author, Integer publicationYear, String classification, String volumeName) {
         this.isbn13 = isbn13;
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
-        this.subjectCode = subjectCode;
+        this.classification = classification;
         this.volumeName = volumeName;
     }
 }
