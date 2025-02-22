781e7999eb6956abb0a68af1f3578e8060788b9f
Jang <sdsd090811@gmail.com>
2025-02-22 17:04:26 +0900

    feat: 도서 정보만 가져오도록 isbn prefix 필터링 기능 추가

--- src/main/java/com/bookspot/batch/step/processor/csv/IsbnValidator.java
+++ src/main/java/com/bookspot/batch/step/processor/csv/IsbnValidator.java
@@ -8,6 +8,9 @@ public class IsbnValidator {
         if(isbn13 == null || isbn13.isBlank() || isbn13.length() != 13)
             return true;
 
+        if(isInvalidPrefix(isbn13))
+            return true;
+
         for (int i = 0; i < isbn13.length(); i++) {
             if (isbn13.charAt(i) < '0' || isbn13.charAt(i) > '9') {
                 return true;
@@ -15,5 +18,10 @@ public class IsbnValidator {
         }
 
         return false;
+    }
+
+    // 8809105873036같은 DVD도 제외된다.
+    private boolean isInvalidPrefix(String isbn13) {
+        return !(isbn13.startsWith("979") || isbn13.startsWith("978"));
     }
 }
