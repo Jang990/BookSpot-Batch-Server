83477fc2db7c78f0ad97ededda02662a9c0ad971
Jang <sdsd090811@gmail.com>
2025-02-27 12:29:59 +0900

    feat: isbn 메모리 관련 처리 Job 추가

--- src/main/java/com/bookspot/batch/job/InMemoryJobConfig.java
+++ src/main/java/com/bookspot/batch/job/InMemoryJobConfig.java
@@ -26,4 +26,20 @@ public class InMemoryJobConfig {
                 .start(inMemoryIsbnIdClearStep)
                 .build();
     }
+
+
+
+    @Bean
+    public Job isbnWarmUpJob(Step inMemoryIsbnWarmUpStep) {
+        return new JobBuilder("inMemoryIsbnWarmUpJob", jobRepository)
+                .start(inMemoryIsbnWarmUpStep)
+                .build();
+    }
+
+    @Bean
+    public Job isbnMemoryClearJob(Step inMemoryIsbnClearStep) {
+        return new JobBuilder("inMemoryIsbnClearJob", jobRepository)
+                .start(inMemoryIsbnClearStep)
+                .build();
+    }
 }
