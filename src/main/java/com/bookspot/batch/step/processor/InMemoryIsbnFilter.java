abf6ff76124f6bc95f5a54fcfe9536a1cae96898
Jang <sdsd090811@gmail.com>
2025-03-05 16:58:39 +0900

    refactor: 목적에 맞지 않는 InMemory필터링 기능을 Isbn필터에서 제거

--- src/main/java/com/bookspot/batch/step/processor/InMemoryIsbnFilter.java
+++ src/main/java/com/bookspot/batch/step/processor/InMemoryIsbnFilter.java
@@ -3,9 +3,11 @@ package com.bookspot.batch.step.processor;
 import com.bookspot.batch.data.file.csv.StockCsvData;
 import com.bookspot.batch.step.service.memory.isbn.IsbnSet;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class InMemoryIsbnFilter implements ItemProcessor<StockCsvData, StockCsvData> {
@@ -13,8 +15,10 @@ public class InMemoryIsbnFilter implements ItemProcessor<StockCsvData, StockCsvD
 
     @Override
     public StockCsvData process(StockCsvData item) throws Exception {
-        if(isbnSet.contains(item.getIsbn()))
+        if (isbnSet.contains(item.getIsbn())) {
+            log.trace("이미 존재하는 ISBN13 -> {}", item);
             return null;
+        }
         return item;
     }
 }
--- src/main/java/com/bookspot/batch/step/processor/IsbnValidationFilter.java
+++ src/main/java/com/bookspot/batch/step/processor/IsbnValidationFilter.java
@@ -2,7 +2,6 @@ package com.bookspot.batch.step.processor;
 
 import com.bookspot.batch.step.processor.csv.IsbnValidator;
 import com.bookspot.batch.data.file.csv.StockCsvData;
-import com.bookspot.batch.step.service.memory.bookid.IsbnMemoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class IsbnValidationFilter implements ItemProcessor<StockCsvData, StockCsvData> {
     private final IsbnValidator isbnValidator;
-    private final IsbnMemoryRepository isbnMemoryRepository;
 
     @Override
     public StockCsvData process(StockCsvData item) throws Exception {
@@ -26,11 +24,6 @@ public class IsbnValidationFilter implements ItemProcessor<StockCsvData, StockCs
             log.info("책 이외의 자료 ISBN -> {}", item);
             return null;
         }*/
-
-        if (isbnMemoryRepository.contains(item.getIsbn())) {
-            log.trace("이미 존재하는 ISBN13 -> {}", item);
-            return null;
-        }
 
         return item;
     }
