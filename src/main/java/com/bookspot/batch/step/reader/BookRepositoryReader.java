8b95b6564c15e86ade4bfc8b661da436f764a38d
Jang <sdsd090811@gmail.com>
2025-03-06 14:26:19 +0900

    feat: 누락된 BookRepositoryReader 추가

---
+++ src/main/java/com/bookspot/batch/step/reader/BookRepositoryReader.java
@@ -0,0 +1,20 @@
+package com.bookspot.batch.step.reader;
+
+import com.bookspot.batch.data.file.csv.ConvertedUniqueBook;
+import com.bookspot.batch.step.service.UniqueBookRepository;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.data.domain.Sort;
+
+import java.util.Collections;
+
+public class BookRepositoryReader extends RepositoryItemReader<ConvertedUniqueBook> {
+    public BookRepositoryReader(
+            UniqueBookRepository repository,
+            final int pageSize) {
+        setName("bookReader");
+        setRepository(repository);
+        setMethodName("findAll");
+        setPageSize(pageSize);
+        setSort(Collections.singletonMap("loanCount", Sort.Direction.DESC));
+    }
+}
