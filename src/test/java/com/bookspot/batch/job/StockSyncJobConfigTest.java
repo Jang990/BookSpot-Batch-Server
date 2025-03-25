43beb14be2aa97b1e1357f2227f7e351fe3883a8
Jang <sdsd090811@gmail.com>
2025-03-25 22:13:55 +0900

    refactor: stockSync 샘플 파일에 도서 정보 디양화

--- src/test/java/com/bookspot/batch/job/StockSyncJobConfigTest.java
+++ src/test/java/com/bookspot/batch/job/StockSyncJobConfigTest.java
@@ -2,6 +2,8 @@ package com.bookspot.batch.job;
 
 import com.bookspot.batch.TestFileUtil;
 import com.bookspot.batch.TestInsertUtils;
+import com.bookspot.batch.data.file.csv.ConvertedUniqueBook;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,7 +17,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -48,6 +55,9 @@ class StockSyncJobConfigTest {
         TestInsertUtils.bookBuilder().id(101L).isbn13("0000000000101").insert(jdbcTemplate);
         TestInsertUtils.bookBuilder().id(102L).isbn13("0000000000102").insert(jdbcTemplate);
         TestInsertUtils.bookBuilder().id(103L).isbn13("0000000000103").insert(jdbcTemplate);
+        TestInsertUtils.bookBuilder().id(104L).isbn13("0000000000104").insert(jdbcTemplate);
+        TestInsertUtils.bookBuilder().id(105L).isbn13("0000000000105").insert(jdbcTemplate);
+        TestInsertUtils.bookBuilder().id(106L).isbn13("0000000000106").insert(jdbcTemplate);
     }
 
     @Test
@@ -56,5 +66,49 @@ class StockSyncJobConfigTest {
         JobExecution jobExecution = jobLauncherTestUtils.launchJob(parameters);
 
         assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+
+        Map<Long, List<Long>> stockResults = findStocks().stream()
+                .collect(Collectors.groupingBy(
+                        StockResult::getLibraryId,
+                        Collectors.mapping(StockResult::getBookId, Collectors.toList())
+                ));
+
+
+        assertThat(stockResults.get(10001L))
+                .containsExactlyInAnyOrderElementsOf(List.of(101L, 102L));
+        assertThat(stockResults.get(10002L))
+                .containsExactlyInAnyOrderElementsOf(List.of(104L, 105L, 106L));
+        assertThat(stockResults.get(10003L))
+                .containsExactlyInAnyOrderElementsOf(List.of(101L, 103L, 105L));
+    }
+
+    private List<StockResult> findStocks() {
+        return jdbcTemplate.query("""
+                        SELECT library_id, book_id
+                        FROM library_stock
+                        """,
+                (rs, rowNum) -> new StockResult(
+                        rs.getLong("library_id"),
+                        rs.getLong("book_id")
+                )
+        );
+    }
+
+    static class StockResult {
+        private Long libraryId;
+        private Long bookId;
+
+        public StockResult(Long libraryId, Long bookId) {
+            this.libraryId = libraryId;
+            this.bookId = bookId;
+        }
+
+        public Long getLibraryId() {
+            return libraryId;
+        }
+
+        public Long getBookId() {
+            return bookId;
+        }
     }
 }
