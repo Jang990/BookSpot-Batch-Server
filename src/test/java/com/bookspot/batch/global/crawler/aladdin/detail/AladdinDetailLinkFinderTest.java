3aba1487e9b36f6bd6356db04d63ef907a868f92
Jang <sdsd090811@gmail.com>
2025-03-25 15:24:34 +0900

    refactor: 알라딘 테스트 코드 주석처리

--- src/test/java/com/bookspot/batch/global/crawler/aladdin/detail/AladdinDetailLinkFinderTest.java
+++ src/test/java/com/bookspot/batch/global/crawler/aladdin/detail/AladdinDetailLinkFinderTest.java
@@ -8,12 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class AladdinDetailLinkFinderTest {
     AladdinDetailLinkFinder finder = new AladdinDetailLinkFinder(new JsoupCrawler());
 
-    @Test
+//    @Test
     void 단일_검색결과가_아니라면_예외발생() {
         assertThrows(IllegalArgumentException.class, () -> finder.find("객체"));
     }
 
-    @Test
+//    @Test
     void 단일_결과가_나오는_ISBN을_주면_책_세부_페이지_링크를_알_수_있다() {
         assertEquals("https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=60550259",
                 finder.find("9788998139766"));
