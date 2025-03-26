5c063d455d9b6d2b70a218daa679e896e0a1a24c
Jang <sdsd090811@gmail.com>
2025-03-26 13:59:01 +0900

    feat: File 경로를 Job 파라미터를 검증하는 JobValidator 추가

---
+++ src/main/java/com/bookspot/batch/job/validator/FilePathJobParameterValidator.java
@@ -0,0 +1,45 @@
+package com.bookspot.batch.job.validator;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
+
+public class FilePathJobParameterValidator implements JobParametersValidator {
+    public static FilePathJobParameterValidator onlyRootDir() {
+        return new FilePathJobParameterValidator(true, false);
+    }
+
+    public static FilePathJobParameterValidator rootDirAndAggregatedFile() {
+        return new FilePathJobParameterValidator(true, true);
+    }
+
+    public static final String AGGREGATED_FILE_PATH = "aggregatedFilePath";
+    public static final String ROOT_DIR_PATH = "rootDirPath";
+
+    private static final String ERROR_MESSAGE_TEMPLATE = "%s 는 필수 JobParameter입니다.";
+
+    private boolean requireRootDirPath;
+    private boolean requireAggregatedFilePath;
+
+    private FilePathJobParameterValidator(boolean requireRootDirPath, boolean requireAggregatedFilePath) {
+        this.requireRootDirPath = requireRootDirPath;
+        this.requireAggregatedFilePath = requireAggregatedFilePath;
+    }
+
+    @Override
+    public void validate(JobParameters parameters) throws JobParametersInvalidException {
+        if (requireAggregatedFilePath)
+            validateFilePath(parameters, AGGREGATED_FILE_PATH);
+
+
+        if (requireRootDirPath)
+            validateFilePath(parameters, ROOT_DIR_PATH);
+    }
+
+    private void validateFilePath(JobParameters parameters, String parameterName) throws JobParametersInvalidException {
+        String filePath = parameters.getString(parameterName);
+
+        if(filePath == null || filePath.isBlank())
+            throw new JobParametersInvalidException(ERROR_MESSAGE_TEMPLATE.formatted(parameterName));
+    }
+}
