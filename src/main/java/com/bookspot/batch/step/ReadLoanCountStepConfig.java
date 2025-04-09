02f21ac1330f02e7d764d205f834f29d364a8ba6
Jang <sdsd090811@gmail.com>
2025-04-09 11:26:06 +0900

    feat: csv파일에서 대출 수를 읽어오는 Step을 멀티스레딩으로 변경

--- src/main/java/com/bookspot/batch/step/ReadLoanCountStepConfig.java
+++ src/main/java/com/bookspot/batch/step/ReadLoanCountStepConfig.java
@@ -1,23 +1,37 @@
 package com.bookspot.batch.step;
 
 import com.bookspot.batch.data.file.csv.StockCsvData;
+import com.bookspot.batch.job.LoanAggregatedJobConfig;
 import com.bookspot.batch.step.listener.StepLoggingListener;
+import com.bookspot.batch.step.partition.StockCsvPartitionConfig;
 import com.bookspot.batch.step.processor.exception.InvalidIsbn13Exception;
-import com.bookspot.batch.step.reader.MultiStockCsvFileReader;
+import com.bookspot.batch.step.reader.StockCsvFileReader;
 import com.bookspot.batch.step.service.memory.loan.InMemoryLoanCountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.partition.support.MultiResourcePartitioner;
+import org.springframework.batch.core.partition.support.TaskExecutorPartitionHandler;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 @RequiredArgsConstructor
 public class ReadLoanCountStepConfig {
-    private static final int BOOK_SYNC_CHUNK_SIZE = 5_000;
+    private static final int CHUNK_SIZE = 5_000;
 
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
@@ -26,15 +40,54 @@ public class ReadLoanCountStepConfig {
     private final StepLoggingListener stepLoggingListener;
 
     @Bean
-    public Step readLoanCountStep(
-            MultiStockCsvFileReader multiBookStockCsvFileReader) {
+    public Step readLoanCountMasterStep(
+            Step readLoanCountStep,
+            TaskExecutorPartitionHandler loanCountPartitionHandler) throws IOException {
+        return new StepBuilder("readLoanCountMasterStep", jobRepository)
+                .partitioner(readLoanCountStep.getName(), loanCountPartitioner(null))
+                .partitionHandler(loanCountPartitionHandler)
+                .build();
+    }
+
+    @Bean
+    public Step readLoanCountStep(StockCsvFileReader stockCsvFileReader) {
         return new StepBuilder("readLoanCountStep", jobRepository)
-                .<StockCsvData, StockCsvData>chunk(BOOK_SYNC_CHUNK_SIZE, platformTransactionManager)
-                .reader(multiBookStockCsvFileReader)
+                .<StockCsvData, StockCsvData>chunk(CHUNK_SIZE, platformTransactionManager)
+                .reader(stockCsvFileReader)
                 .writer(memoryIsbnWriter())
                 .listener(stepLoggingListener)
                 .build();
     }
+
+    @Bean
+    public TaskExecutorPartitionHandler loanCountPartitionHandler(
+            Step readLoanCountStep,
+            TaskExecutor stockCsvTaskPool) {
+        TaskExecutorPartitionHandler partitionHandler = new TaskExecutorPartitionHandler();
+        partitionHandler.setStep(readLoanCountStep);
+        partitionHandler.setTaskExecutor(stockCsvTaskPool);
+        return partitionHandler;
+    }
+
+    @Bean
+    @StepScope
+    public MultiResourcePartitioner loanCountPartitioner(
+            @Value(LoanAggregatedJobConfig.DIRECTORY_PATH) String sourceDir) throws IOException {
+        MultiResourcePartitioner partitioner = new MultiResourcePartitioner();
+
+        Path rootPath = Paths.get(sourceDir);
+        Resource[] resources = Files.list(rootPath) // 루트 디렉토리의 파일 리스트 가져오기
+                .filter(Files::isRegularFile) // 파일만 필터링 (디렉토리 제외)
+                .map(Path::toFile) // Path -> File 변환
+                .map(FileSystemResource::new) // File -> Resource 변환
+                .toArray(Resource[]::new);
+
+        partitioner.setKeyName(StockCsvPartitionConfig.PARTITIONER_KEY);
+        partitioner.setResources(resources);
+
+        return partitioner;
+    }
+
 
     // 새로 등록된 책을 메모리에 등록
     @Bean
