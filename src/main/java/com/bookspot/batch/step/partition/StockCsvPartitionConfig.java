86afb9cb0e59610f16f4c513dd8023f677edf3d0
Jang <sdsd090811@gmail.com>
2025-04-18 20:22:07 +0900

    refactor: 사용하지 않는 파일관련 job 파라미터 validator 제거

--- src/main/java/com/bookspot/batch/step/partition/StockCsvPartitionConfig.java
+++ src/main/java/com/bookspot/batch/step/partition/StockCsvPartitionConfig.java
@@ -1,20 +1,7 @@
 package com.bookspot.batch.step.partition;
 
-import com.bookspot.batch.job.validator.FilePathJobParameterValidator;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.partition.support.MultiResourcePartitioner;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Configuration
 public class StockCsvPartitionConfig {
