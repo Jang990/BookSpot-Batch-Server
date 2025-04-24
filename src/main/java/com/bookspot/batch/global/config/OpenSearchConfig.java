5fc1d539b59fc77b9451902ed6785b0801e48d8d
Jang <sdsd090811@gmail.com>
2025-04-24 16:22:10 +0900

    feat: opensearch 설정 추가

---
+++ src/main/java/com/bookspot/batch/global/config/OpenSearchConfig.java
@@ -0,0 +1,55 @@
+package com.bookspot.batch.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.json.jackson.JacksonJsonpMapper;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.rest_client.RestClientTransport;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenSearchConfig {
+    @Value("${opensearch.serverUrl}")
+    private String serverUrl;
+
+    @Value("${opensearch.username}")
+    private String username;
+
+    @Value("${opensearch.password}")
+    private String password;
+
+
+    @Bean
+    public OpenSearchClient openSearchClient() {
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(
+                AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password)
+        );
+
+        return new OpenSearchClient(
+                new RestClientTransport(
+                        RestClient.builder(new HttpHost(serverUrl))
+                                .setHttpClientConfigCallback(
+                                        httpClientBuilder -> httpClientBuilder
+                                                .setDefaultCredentialsProvider(credentialsProvider)
+                                                .setDefaultIOReactorConfig(
+                                                        IOReactorConfig.custom()
+                                                                .setIoThreadCount(1)
+                                                                .build()
+                                                )
+                                )
+                                .build(),
+                        new JacksonJsonpMapper(new ObjectMapper())
+                )
+        );
+    }
+
+}
