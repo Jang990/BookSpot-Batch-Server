190eaf1b54db31a6989d6053f166141dc1fef5f8
Jang <sdsd090811@gmail.com>
2025-06-21 16:01:12 +0900

    feat: bookCode를 풀어주는 Resolver 추가

---
+++ src/main/java/com/bookspot/batch/step/service/BookCodeResolver.java
@@ -0,0 +1,45 @@
+package com.bookspot.batch.step.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BookCodeResolver {
+    private final Map<Integer, String> bookCodeMap;
+    private static final int CODE_MIN = 0, CODE_MAX = 999;
+    private static final String FORMAT = "%03d.%s";
+    private static final String EMPTY = "EMPTY";
+
+    public String[] resolve(final int bookCode) {
+        if(bookCode < CODE_MIN || CODE_MAX < bookCode)
+            throw new IllegalArgumentException("도서의 분류 코드는 0~999사이");
+
+        int top    = (bookCode / 100) * 100;
+        int middle = (bookCode / 10)  * 10;
+        int leaf = bookCode;
+        return Stream.of(top, middle, leaf)
+                .distinct()
+                .map(this::format)
+                .filter(s -> {
+                    boolean isValidCode = !s.equals(EMPTY);
+                    if(!isValidCode)
+                        log.trace("존재하지 않는 책 코드 : {}", bookCode);
+                    return isValidCode;
+                })
+                .toArray(String[]::new);
+    }
+
+    private String format(int bookCode) {
+        String codeName = bookCodeMap.get(bookCode);
+        if (codeName == null) {
+            return EMPTY;
+        }
+        return FORMAT.formatted(bookCode, codeName);
+    }
+}
---
+++ src/test/java/com/bookspot/batch/step/service/BookCodeResolverTest.java
@@ -0,0 +1,72 @@
+package com.bookspot.batch.step.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookCodeResolverTest {
+    private static Map<Integer, String> sampleMap() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(0, "총류");
+            map.put(1, "지식 및 학문 일반");
+            map.put(10, "도서학, 서지학");
+                map.put(19, "장서목록");
+            map.put(30, "백과사전");
+                map.put(32, "중국어");
+                map.put(33, "일본어");
+        map.put(100, "철학");
+            map.put(102, "잡저");
+            map.put(110, "형이상학");
+                map.put(111, "방법론");
+                map.put(112, "존재론");
+            map.put(120, "인식론, 인과론, 인간학");
+                map.put(123, "자유 및 필연");
+                map.put(124, "목적론");
+
+        return map;
+    }
+
+    BookCodeResolver resolver = new BookCodeResolver(sampleMap());
+
+    @Test
+    void 분류코드는_0_999사이_숫자() {
+        assertThrows(IllegalArgumentException.class, () -> resolver.resolve(-1));
+        assertThrows(IllegalArgumentException.class, () -> resolver.resolve(1000));
+        resolver.resolve(0);
+        resolver.resolve(500);
+        resolver.resolve(999);
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    void 분류코드를_풀어줌(int bookCode, String[] expected) {
+        assertThat(resolver.resolve(bookCode))
+                .containsExactlyInAnyOrder(expected);
+    }
+
+    static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(0, new String[]{"000.총류"}),
+                Arguments.of(1, new String[]{"000.총류", "001.지식 및 학문 일반"}),
+                Arguments.of(19, new String[]{"000.총류", "010.도서학, 서지학", "019.장서목록"}),
+                Arguments.of(32, new String[]{"000.총류", "030.백과사전", "032.중국어"}),
+                Arguments.of(33, new String[]{"000.총류", "030.백과사전", "033.일본어"}),
+                Arguments.of(100, new String[]{"100.철학"}),
+                Arguments.of(102, new String[]{"100.철학","102.잡저"}),
+                Arguments.of(110, new String[]{"100.철학","110.형이상학"}),
+                Arguments.of(112, new String[]{"100.철학","110.형이상학","112.존재론"}),
+                Arguments.of(120, new String[]{"100.철학","120.인식론, 인과론, 인간학"}),
+                Arguments.of(124, new String[]{"100.철학","120.인식론, 인과론, 인간학", "124.목적론"}),
+                Arguments.of(125, new String[]{"100.철학","120.인식론, 인과론, 인간학" /* 존재하지 않음 */})
+        );
+    }
+
+}
