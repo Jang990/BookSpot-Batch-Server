16dc0488239d15f18a2e95baabfbc07422a47a48
Jang <sdsd090811@gmail.com>
2025-08-03 14:57:29 +0900

    refactor: Job 파라미터 작업을 클래스로 분리

---
+++ src/main/java/com/bookspot/batch/job/launcher/BookSpotJobParamBuilder.java
@@ -0,0 +1,69 @@
+package com.bookspot.batch.job.launcher;
+
+import com.bookspot.batch.global.properties.files.BookSpotDirectoryProperties;
+import com.bookspot.batch.global.properties.files.BookSpotFileProperties;
+import com.bookspot.batch.job.BookSpotParentJobConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+@RequiredArgsConstructor
+public class BookSpotJobParamBuilder {
+    private final BookSpotDirectoryProperties directoryProperties;
+    private final BookSpotFileProperties fileProperties;
+
+    private JobParametersBuilder commonBuilder() {
+        JobParametersBuilder builder = new JobParametersBuilder();
+        builder.addLocalDate(
+                BookSpotParentJobConfig.MONTH_PARAM_NAME,
+                monthParam()
+        );
+        return builder;
+    }
+
+    private LocalDate monthParam() {
+        LocalDate now = LocalDate.now();
+        return LocalDate.of(now.getYear(), now.getMonth(), 1);
+    }
+
+    public JobParameters buildBookSpotParams() {
+        return commonBuilder()
+                .addString(
+                        BookSpotParentJobConfig.LIBRARY_FILE_PARAM_NAME,
+                        fileProperties.library()
+                )
+                .addString(
+                        BookSpotParentJobConfig.STOCK_DIR_PARAM_NAME,
+                        directoryProperties.bookSync()
+                )
+                .addString(
+                        BookSpotParentJobConfig.DOWNLOAD_DIR_PARAM_NAME,
+                        directoryProperties.bookSync()
+                )
+                .addString(
+                        BookSpotParentJobConfig.LOAN_OUTPUT_FILE_PARAM_NAME,
+                        fileProperties.loan()
+                )
+                .addString(
+                        BookSpotParentJobConfig.CLEANSING_DIR_PARAM_NAME,
+                        directoryProperties.cleansingStock()
+                )
+                .addString(
+                        BookSpotParentJobConfig.DUPLICATED_FILTER_DIR_PARAM_NAME,
+                        directoryProperties.filteredStock()
+                )
+                .addString(
+                        BookSpotParentJobConfig.DELETE_DIR_PARAM_NAME,
+                        directoryProperties.deletedStock()
+                )
+                .toJobParameters();
+    }
+
+    public JobParameters buildOpenSearchParams() {
+        return commonBuilder().toJobParameters();
+    }
+}
--- src/main/java/com/bookspot/batch/job/launcher/MyBatchJobLauncher.java
+++ src/main/java/com/bookspot/batch/job/launcher/MyBatchJobLauncher.java
@@ -1,7 +1,6 @@
 package com.bookspot.batch.job.launcher;
 
 import com.bookspot.batch.global.config.TaskExecutorConfig;
-import com.bookspot.batch.job.BookSpotParentJobConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.*;
@@ -10,8 +9,6 @@ import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteExcep
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -19,15 +16,7 @@ public class MyBatchJobLauncher {
     private final JobLauncher jobLauncher;
 
     @Async(TaskExecutorConfig.SCHEDULING_TASK_POOL_NAME)
-    public void launch(Job job, JobParametersBuilder builder) {
-        LocalDate now = LocalDate.now();
-        builder.addLocalDate(
-                BookSpotParentJobConfig.MONTH_PARAM_NAME,
-                LocalDate.of(now.getYear(), now.getMonth(), 1)
-        );
-
-        JobParameters jobParameters = builder.toJobParameters();
-
+    public void launch(Job job, JobParameters jobParameters) {
         try {
             JobExecution jobExecution = jobLauncher.run(job, jobParameters);
             if (isCompleted(jobExecution.getExitStatus())) {
