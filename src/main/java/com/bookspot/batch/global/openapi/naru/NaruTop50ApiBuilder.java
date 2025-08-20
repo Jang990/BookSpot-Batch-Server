d16861f7e04881ec089e62d7327e4f4d39c540aa
Jang <sdsd090811@gmail.com>
2025-08-20 19:50:24 +0900

    feat: 정보나루 top 50 요청 api 빌더에 조건에 따른 api 파라미터 변경 로직 추가

--- src/main/java/com/bookspot/batch/global/openapi/naru/NaruTop50ApiBuilder.java
+++ src/main/java/com/bookspot/batch/global/openapi/naru/NaruTop50ApiBuilder.java
@@ -1,5 +1,7 @@
 package com.bookspot.batch.global.openapi.naru;
 
+import com.bookspot.batch.data.document.RankingAge;
+import com.bookspot.batch.data.document.RankingGender;
 import com.bookspot.batch.data.document.RankingType;
 import com.bookspot.batch.step.reader.api.top50.RankingConditions;
 
@@ -13,6 +15,11 @@ public class NaruTop50ApiBuilder {
     private final String format = "json";
     private final LocalDate periodStart;
     private final LocalDate periodEnd;
+
+    private final Integer ageStart;
+    private final Integer ageEnd;
+
+    private final Integer genderCode;
 
     public NaruTop50ApiBuilder(
             String apiUrl,
@@ -33,6 +40,24 @@ public class NaruTop50ApiBuilder {
             periodEnd = referenceDate.with(DayOfWeek.SUNDAY);
         }
 
+        if (RankingAge.ALL.equals(rankingConditions.age())) {
+            ageStart = null;
+            ageEnd = null;
+        } else {
+            ageStart = rankingConditions.age().getStart();
+            ageEnd = rankingConditions.age().getEnd();
+        }
+
+        this.genderCode = getGenderCode(rankingConditions.gender());
+
+    }
+
+    private Integer getGenderCode(RankingGender gender) {
+        return switch (gender) {
+            case ALL -> null;
+            case MALE -> 0;
+            case FEMALE -> 1;
+        };
     }
 
     private boolean isMonthly(RankingType rankingType) {
@@ -40,9 +65,27 @@ public class NaruTop50ApiBuilder {
     }
 
     public String build() {
-        return apiUrl + "?"
-                + "format=" + format
-                + "&startDt=" + periodStart.toString()
-                + "&endDt=" + periodEnd.toString();
+        StringBuilder sb = new StringBuilder(apiUrl);
+        sb.append("?format=").append(format);
+        appendParam(sb, "startDt", periodStart.toString());
+        appendParam(sb, "endDt", periodEnd.toString());
+
+        appendParam(sb, "from_age", ageStart);
+        appendParam(sb, "to_age", ageEnd);
+        appendParam(sb, "gender", genderCode);
+
+        return sb.toString();
+    }
+
+    private void appendParam(StringBuilder sb, String key, String val) {
+        if (val == null)
+            return;
+        sb.append("&").append(key).append("=").append(val);
+    }
+
+    private void appendParam(StringBuilder sb, String key, Integer val) {
+        if(val == null)
+            return;
+        sb.append("&").append(key).append("=").append(val);
     }
 }
--- src/test/java/com/bookspot/batch/global/openapi/naru/NaruTop50ApiBuilderTest.java
+++ src/test/java/com/bookspot/batch/global/openapi/naru/NaruTop50ApiBuilderTest.java
@@ -5,26 +5,65 @@ import com.bookspot.batch.data.document.RankingGender;
 import com.bookspot.batch.data.document.RankingType;
 import com.bookspot.batch.step.reader.api.top50.RankingConditions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NaruTop50ApiBuilderTest {
-    @Test
-    void 월간_top50_api() {
-        LocalDate referenceDate = LocalDate.of(2025, 8, 15);
-        RankingConditions conditions = new RankingConditions(
-                RankingType.MONTHLY,
-                RankingGender.ALL,
-                RankingAge.ALL
-        );
+    private static final LocalDate referenceDate = LocalDate.of(2025, 8, 15);
+    private static final String baseUrl = "TEST_URL";
 
-        NaruTop50ApiBuilder builder = new NaruTop50ApiBuilder("AAA", referenceDate, conditions);
+    @ParameterizedTest(name = "{1}")
+    @MethodSource("args")
+    void top50_api(String expectedUrl, RankingConditions conditions) {
+        NaruTop50ApiBuilder builder = new NaruTop50ApiBuilder(baseUrl, referenceDate, conditions);
+
         assertEquals(
-                "AAA?format=json&startDt=2025-08-01&endDt=2025-08-31",
+                expectedUrl,
                 builder.build()
         );
     }
+
+    static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(
+                        "TEST_URL?format=json&startDt=2025-08-01&endDt=2025-08-31",
+                        new RankingConditions(RankingType.MONTHLY, RankingGender.ALL, RankingAge.ALL)
+                ),
+                Arguments.of(
+                        "TEST_URL?format=json&startDt=2025-08-11&endDt=2025-08-17",
+                        new RankingConditions(RankingType.WEEKLY, RankingGender.ALL, RankingAge.ALL)
+                ),
+
+                Arguments.of(
+                        "TEST_URL?format=json&startDt=2025-08-11&endDt=2025-08-17&gender=0",
+                        new RankingConditions(RankingType.WEEKLY, RankingGender.MALE, RankingAge.ALL)
+                ),
+                Arguments.of(
+                        "TEST_URL?format=json&startDt=2025-08-11&endDt=2025-08-17&gender=1",
+                        new RankingConditions(RankingType.WEEKLY, RankingGender.FEMALE, RankingAge.ALL)
+                ),
+
+                Arguments.of(
+                        "TEST_URL?format=json&startDt=2025-08-11&endDt=2025-08-17&from_age=0&to_age=14",
+                        new RankingConditions(RankingType.WEEKLY, RankingGender.ALL, RankingAge.AGE_0_14)
+                ),
+                Arguments.of(
+                        "TEST_URL?format=json&startDt=2025-08-11&endDt=2025-08-17&from_age=20&to_age=29",
+                        new RankingConditions(RankingType.WEEKLY, RankingGender.ALL, RankingAge.AGE_20_29)
+                ),
+                Arguments.of(
+                        "TEST_URL?format=json&startDt=2025-08-11&endDt=2025-08-17&from_age=50",
+                        new RankingConditions(RankingType.WEEKLY, RankingGender.ALL, RankingAge.AGE_50_UP)
+                )
+        );
+    }
+
+
 
 }
