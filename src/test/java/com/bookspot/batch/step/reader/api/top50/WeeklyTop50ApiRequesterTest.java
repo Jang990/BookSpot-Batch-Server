41deeadb2535f244ad53de7504d20436fdfa1dc7
Jang <sdsd090811@gmail.com>
2025-08-20 14:07:54 +0900

    test: 8/18일 전 주의 대출 top50 도서 검증 테스트 추가

--- src/test/java/com/bookspot/batch/step/reader/api/top50/WeeklyTop50ApiRequesterTest.java
+++ src/test/java/com/bookspot/batch/step/reader/api/top50/WeeklyTop50ApiRequesterTest.java
@@ -1,12 +1,15 @@
 package com.bookspot.batch.step.reader.api.top50;
 
 import com.bookspot.batch.data.Top50Book;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 //@SpringBootTest
 class WeeklyTop50ApiRequesterTest {
@@ -15,11 +18,17 @@ class WeeklyTop50ApiRequesterTest {
     WeeklyTop50ApiRequester requester;
 
 //    @Test
-    void test() {
+    void 주간_대출_수_top50_API_검증() {
         List<Top50Book> result = requester.findTop50(LocalDate.of(2025, 8, 18));
-        for (Top50Book doc : result) {
-            System.out.println(doc);
-        }
+        assertEquals(50, result.size());
+
+        assertEquals("소년이 온다 :한강 장편소설", result.getFirst().title());
+        assertEquals(1, result.getFirst().ranking());
+        assertEquals("9788936434120", result.getFirst().isbn13());
+
+        assertEquals("흔한남매 이상한 나라의 고전 읽기", result.getLast().title());
+        assertEquals(50, result.getLast().ranking());
+        assertEquals("9791168416314", result.getLast().isbn13());
     }
 
 }
