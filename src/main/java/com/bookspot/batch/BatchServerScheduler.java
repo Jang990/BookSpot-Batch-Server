387d57960dbfe387cc79036205c87eaf0a70a5b2
Jang <sdsd090811@gmail.com>
2025-08-31 13:15:18 +0900

    fix: 불필요한 웜업 스케줄러 제거

--- src/main/java/com/bookspot/batch/BatchServerScheduler.java
+++ src/main/java/com/bookspot/batch/BatchServerScheduler.java
@@ -1,24 +1,15 @@
 package com.bookspot.batch;
 
-import com.bookspot.batch.data.document.RankingAge;
-import com.bookspot.batch.data.document.RankingGender;
-import com.bookspot.batch.data.document.RankingType;
 import com.bookspot.batch.job.launcher.CustomJobLauncher;
 import com.bookspot.batch.job.listener.alert.JobAlertMessageConvertor;
-import com.bookspot.batch.service.SimpleRequester;
 import com.bookspot.batch.service.alert.SlackAlertService;
-import com.bookspot.batch.step.reader.api.top50.RankingConditions;
 import com.bookspot.batch.web.JobStatusService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.ResourceAccessException;
 
-import java.time.LocalDate;
-import java.util.List;
 
 /**
  * @see com.bookspot.batch.global.config.TaskExecutorConfig
@@ -32,7 +23,6 @@ public class BatchServerScheduler {
     private final CustomJobLauncher customJobLauncher;
     private final SlackAlertService slackAlertService;
     private final JobAlertMessageConvertor convertor;
-    private final SimpleRequester simpleRequester;
 
     @Value("${backend.url}")
     private String backendUrl;
@@ -71,42 +61,5 @@ public class BatchServerScheduler {
                     )
             );
         }
-    }
-
-    @Scheduled(cron = "0 */15 * * * *", zone = "Asia/Seoul")
-    public void warmUpBackend() {
-        List<String> urls = List.of(
-                backendUrl.concat("/api/books?"),
-                backendUrl.concat("/api/books?title=한강"),
-                backendUrl.concat("/api/books?title=돼지"),
-                backendUrl.concat("/api/books?libraryId=682"),
-                backendUrl.concat("/api/books?title=객체&categoryId=5&categoryLevel=LEAF")
-        );
-
-        int failed = 0;
-        for (String url : urls) {
-            try {
-                HttpStatusCode status = simpleRequester.sendGetRequest(url);
-                if (!status.is2xxSuccessful())
-                    failed++;
-            } catch (ResourceAccessException e) {
-                failed++;
-            }
-        }
-
-        if (failed >= 3) {
-            slackAlertService.error(
-                    convertor.convertSimple(
-                            "Backend 웜업 스케줄러",
-                            "Backend 문제 발생",
-                            "%d개의 요청 중 %d개의 요청 실패".formatted(urls.size(), failed)
-                    )
-            );
-            log.error("Backend 웜업 작업 실패. - {}개의 요청 중 {}개의 요청 실패", urls.size(), failed);
-            return;
-        }
-        
-        if(failed > 0)
-            log.info("Backend 웜업 작업 완료. - {}개의 요청 중 {}개의 요청 실패", urls.size(), failed);
     }
 }
