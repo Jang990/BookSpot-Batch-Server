10f02bcd8e14964c4a43cc37730cde3409fb2119
Jang <sdsd090811@gmail.com>
2025-08-31 13:08:56 +0900

    feat: alter가 prod환경에서만 동작하도록 변경

--- src/main/java/com/bookspot/batch/job/listener/alert/AlertJobListener.java
+++ src/main/java/com/bookspot/batch/job/listener/alert/AlertJobListener.java
@@ -1,25 +1,29 @@
 package com.bookspot.batch.job.listener.alert;
 
-import com.bookspot.batch.service.alert.AlertBody;
 import com.bookspot.batch.service.alert.AlertMessage;
 import com.bookspot.batch.service.alert.SlackAlertService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
 public class AlertJobListener implements JobExecutionListener {
     private final SlackAlertService alertService;
     private final JobAlertMessageConvertor messageFormatter;
+    private final Environment env;
 
     @Override
     public void afterJob(JobExecution jobExecution) {
+        if (!Arrays.asList(env.getActiveProfiles()).contains("prod")) {
+            return;
+        }
+
         AlertMessage alertMessage = messageFormatter.convert(jobExecution);
 
         if (BatchStatus.COMPLETED.equals(jobExecution.getStatus())) {
--- src/main/java/com/bookspot/batch/step/listener/alert/AlertStepListener.java
+++ src/main/java/com/bookspot/batch/step/listener/alert/AlertStepListener.java
@@ -7,16 +7,25 @@ import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
 public class AlertStepListener implements StepExecutionListener {
     private final SlackAlertService alertService;
     private final StepAlertMessageConvertor alertMessageConvertor;
+    private final Environment env;
 
     @Override
     public ExitStatus afterStep(StepExecution stepExecution) {
+        if (!Arrays.asList(env.getActiveProfiles()).contains("prod")) {
+            return StepExecutionListener.super.afterStep(stepExecution);
+        }
+
+
         AlertMessage alertMessage = alertMessageConvertor.convert(stepExecution);
 
         if (BatchStatus.COMPLETED.equals(stepExecution.getStatus())) {
