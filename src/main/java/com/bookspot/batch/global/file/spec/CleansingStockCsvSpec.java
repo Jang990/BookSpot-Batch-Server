ffaa12ef7b54e30db467672a308cc60eb650cb88
Jang <sdsd090811@gmail.com>
2025-09-11 16:43:58 +0900

    feat: 정제된 StockCsvSpec에서 readLine을 할 수 있도록 변경

--- src/main/java/com/bookspot/batch/global/file/spec/CleansingStockCsvSpec.java
+++ src/main/java/com/bookspot/batch/global/file/spec/CleansingStockCsvSpec.java
@@ -1,5 +1,8 @@
 package com.bookspot.batch.global.file.spec;
 
+import com.bookspot.batch.data.LibraryStock;
+import org.springframework.batch.item.file.transform.FieldSet;
+
 public enum CleansingStockCsvSpec {
     BOOK_ID("bookId"),
     LIBRARY_ID("libraryId"),
@@ -13,6 +16,17 @@ public enum CleansingStockCsvSpec {
 
     public String value() {
         return fieldName;
+    }
+
+    public static LibraryStock readLine(FieldSet fieldSet) {
+        long libraryId = fieldSet.readLong(LIBRARY_ID.value());
+        long bookId = fieldSet.readLong(BOOK_ID.value());
+        String subjectCode = fieldSet.readString(SUBJECT_CODE.value());
+        return new LibraryStock(
+                libraryId,
+                bookId,
+                subjectCode.isBlank() ? null : subjectCode
+        );
     }
 
     public static String createLine(long bookId, long libraryId, String subjectCode) {
